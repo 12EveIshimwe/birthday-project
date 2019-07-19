f29ba7774cab3680374109b920dfe6218ff644b5
Eve Ishimwe <eveishimwe1@gmail.com>
2019-07-19 18:31:42 +0200

    addeda variable in javascript.

--- javascript.js
+++ javascript.js
@@ -4,9 +4,9 @@ function birth() {
     var y = parseFloat(document.getElementById('year').value);
     var c = (y - 1) / 100 + 1;
     var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
-    // var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     console.log(dayoftheweek);
-    document.getElementById('result').innerHTML = Math.round(dayoftheweek);
+    document.getElementById('result').innerHTML = day[Math.round(dayoftheweek)];
     var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var choice = document.getElementById('select').value;
