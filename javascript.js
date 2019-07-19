029e73818bde969a74dd336af0de4b72a9e9ddf7
Eve Ishimwe <eveishimwe1@gmail.com>
2019-07-19 17:13:02 +0200

    made changes on javascript.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function birth() {
-    var d = parseFloat(document.getElementById('d').value);
-    var m = parseFloat(document.getElementById('m').value);
-    var y = parseFloat(document.getElementById('y').value);
+    var d = parseFloat(document.getElementById('day').value);
+    var m = parseFloat(document.getElementById('month').value);
+    var y = parseFloat(document.getElementById('year').value);
     var c = (y - 1) / 100 + 1;
     var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
     var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
