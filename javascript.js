cda052c8d3a8a9d628a5910d208ee612ae0df614
Eve Ishimwe <eveishimwe1@gmail.com>
2019-07-19 12:00:40 +0200

    I added console log in javascript.

--- javascript.js
+++ javascript.js
@@ -3,4 +3,7 @@ function birth {
     var month = parseFloat(document.getElementById('month').value);
     var year = parseFloat(document.getElementById('day').value);
     var c = (y - 1) / 100 + 1;
-}
+    var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
+    console.log(dayoftheweek);
+    document.getElementById('result').innerHTML = Math.round(dayof the week);
+    va
