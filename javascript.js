dc4a2eacf8d7e04b5240ba5e3f409b579eb12389
Eve Ishimwe <eveishimwe1@gmail.com>
2019-07-19 20:10:21 +0200

    added an if in javascript.

--- javascript.js
+++ javascript.js
@@ -10,6 +10,17 @@ function birth() {
     var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var choice = document.getElementById('select').value;
+    if (d<31 || d>1){
+        alert("The inputted day doesn't exist");
+    }
+    if (m<12 || m>1){
+        alert("The inputted month doesn't exist");
+    }
+    if (y<2019 || y>1){
+        alert("The inputted year doesn't exist");
+    }
+
+
     if (choice === "female") {
         console.log(female[Math.round(dayoftheweek)]);
         document.getElementById ('Akan').innerHTML = female[Math.round(dayoftheweek)];
@@ -21,4 +32,16 @@ function birth() {
     else {
         console.log("Invalid output");
     }
+    if (choice === "female" && (DD>0 && DD<=31)&&(MM>1 && MM<=31)&&YY>=1) {
+        console.log(female[Math.round(dayoftheweek)]);
+        document.getElementById ('Akan').innerHTML = female[Math.round(dayoftheweek)];
+    }
+    else if (choice === "male" && (DD>0 && DD<=31)&&(MM>1 && MM<=31)&&YY>=1) {
+        console.log(male[Math.round(dayoftheweek)]);
+        document.getElementById('Akan').innerHTML = male[Math.round(dayoftheweek)];
+    }
+    else {
+        console.log alert(("Invalid output"));
+    }
+
 }
