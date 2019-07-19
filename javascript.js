a3dc7a57c1bb190a02276cf8b59390066b69e561
Eve Ishimwe <eveishimwe1@gmail.com>
2019-07-19 11:52:52 +0200

    i created function.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
-// function hhhhh {
-//     var parseInt();
-// }
+function birth {
+    var day = parseFloat(document.getElementById('day').value);
+    var month = parseFloat(document.getElementById('month').value);
+    var year = parseFloat(document.getElementById('day').value);
+    var c = (y - 1) / 100 + 1;
+}
