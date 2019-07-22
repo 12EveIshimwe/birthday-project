fbcb15e05c08456ff112fa8e439de2462767dc6c
Eve Ishimwe <eveishimwe1@gmail.com>
2019-07-22 12:53:37 +0200

    made changes.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+alert("Welcome to Akan Names Webpage!");
 function akan() {
     var d = parseFloat(document.getElementById('day').value);
     var m = parseFloat(document.getElementById('month').value);
@@ -6,20 +7,23 @@ function akan() {
     var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
     var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     console.log(dayoftheweek);
-    document.getElementById('result').innerHTML = day[Math.round(dayoftheweek)];
     var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var choice = document.getElementById('select').value;
+    var choice = document.querySelector('input[name="gender"]:checked').value;
+    console.log(choice);
     if (d>31 || d<1){
-        alert("The inputted day doesn't exist");
+        document.getElementById("d").innerHTML = ("The input day doesn't exist");
     }
-    if (m>12 || m<1){
-        alert("The inputted month doesn't exist");
+    else if (m>12 || m<1){
+        document.getElementById("m").innerHTML = ("The input month doesn't exist");
     }
-    if (y>2019 || y<1){
-        alert("The inputted year doesn't exist");
+    else if (y>2019 || y<1){
+        document.getElementById("y").innerHTML = ("The input year doesn't exist");
     }
-    if (choice === "female") {
+    else{ 
+        document.getElementById('result').innerHTML = day[Math.round(dayoftheweek)];
+
+    if (choice === "female" ) {
         console.log(female[Math.round(dayoftheweek)]);
         document.getElementById ('Akan').innerHTML = female[Math.round(dayoftheweek)];
     }
@@ -30,16 +34,5 @@ function akan() {
     else {
         console.log("Invalid output");
     }
-    // if (choice === "female" && (DD>0 && DD<=31)&&(MM>1 && MM<=31)&&YY>=1) {
-    //     console.log(female[Math.round(dayoftheweek)]);
-    //     document.getElementById ('Akan').innerHTML = female[Math.round(dayoftheweek)];
-    // }
-    // else if (choice === "male" && (DD>0 && DD<=31)&&(MM>1 && MM<=31)&&YY>=1) {
-    //     console.log(male[Math.round(dayoftheweek)]);
-    //     document.getElementById('Akan').innerHTML = male[Math.round(dayoftheweek)];
-    // }
-    // else {
-    //     console.log alert(("Invalid output"));
-    // }
-
+}
 }
